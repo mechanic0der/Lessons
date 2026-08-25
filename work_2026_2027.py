c092218844df7da1290acd434aab2d1979c46154
artem <superartem1301@gmail.com>
2026-08-25 12:16:38 +0300

    занятие 1

---
+++ work_2026_2027.py
@@ -0,0 +1,16 @@
+f = "return"
+print(f[1:])
+d = "Артемаааааа ааааа аааааааа".split(" ")
+print(d)
+age = int(input())
+status = "взрослый" if age >= 18 else "ребёнок"
+print(status)
+for i in range(6):
+    if i == 3:
+        continue
+    print(i)
+g = ['6','7']
+for i in range(len(g)):
+    print(g[i])
+
+
